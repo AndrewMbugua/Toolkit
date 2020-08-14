e13cb7ca7c9f7842863f12fadda2da75a80e3a68
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-08-14 19:54:08 +0300

    Add files via upload

---
+++ honey_pot_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#To run this you have to have root privileges
+
+#This aims to use the tool Petbox to automate honey port setups
+
+#Log in to your Kali Linux machine as an admin user. Open a terminal window and download pentbox with the command: 
+#Fetch the tool
+#save and run in root folder
+
+eval "cd /root/"
+eval "wget http://downloads.sourceforge.net/project/pentbox18realised/pentbox-1.8.tar.gz"
+
+eval "tar xvfz pentbox-1.8.tar.gz"
+
+eval "cd /root/pentbox-1.8 && ./pentbox.rb"
+
